75346b0c928e0a0b84e35251def72ff7c211ede6
J. Alejandro Cardona <alejandrocardona@outlook.com>
2015-10-17 10:25:47 -0500

    Homework of module 03 done

--- Restaurant Manager/RestaurantManager.universalwindows/MainPage.xaml.cs
+++ Restaurant Manager/RestaurantManager.universalwindows/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +10,16 @@ namespace RestaurantManager.universalwindows
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
-        {
+        public MainPage() {
             this.InitializeComponent();
+        }
+
+        private void Button_Click_Order(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(ExpeditePage));
+        }
+
+        private void Button_Click_Submit(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(OrderPage));
         }
     }
 }
--- Restaurant Manager/RestaurantManager.universalwindows/OrderPage.xaml.cs
+++ Restaurant Manager/RestaurantManager.universalwindows/OrderPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,6 +12,10 @@ namespace RestaurantManager.universalwindows
     {
         public OrderPage() {
             this.InitializeComponent();
+        }
+
+        private void AppBarButton_Click_Home(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
